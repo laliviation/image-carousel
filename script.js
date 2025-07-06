11656ed1093b8c038dcd97a532ede53356f812e2
laliviation <154061022+laliviation@users.noreply.github.com>
2025-07-06 12:33:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const images = ["slide1.png", "slide2.png", "slide3.png", "slide4.png", "slide5.png", "slide6.png", "slide7.png", "slide8.png", "slide9.png", "slide10.png"];
 const links = ["https://boresjazz.hu/programok/?render=full", "https://opusjazzclub.hu/koncertek/jambor-armin-quartet-2508", "https://www.facebook.com/people/Jazz-PArt/100084734805068/", "https://www.jazzland.at", "https://www.musikwerkstattwels.at", "https://www.lepirate-rosenheim.de", "https://www.royalgarden.at/programm/", "https://www.tribuene-linz.at", "https://www.cafe-traxlmayr.at/konzerte/", "https://zehnerhaus-badradkersburg.at/veranstaltungskalender/"];
-let current = 0
+
+let current = 0;
 
 const carouselImage = document.getElementById("carouselImage");
-
-// Initialize with first image
 carouselImage.src = images[current];
 
 carouselImage.addEventListener("click", function(event) {
@@ -20,7 +19,7 @@ carouselImage.addEventListener("click", function(event) {
     current = (current + 1) % images.length;
     carouselImage.src = images[current];
   } else {
-    // Middle 40%: Open link
+    // Middle 40%: Open link in new tab
     window.open(links[current], "_blank");
   }
 });
