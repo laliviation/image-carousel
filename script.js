2c3d0c042118116c7f708fd7f35c2f31e48a0fc0
laliviation <154061022+laliviation@users.noreply.github.com>
2025-07-06 12:44:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
-const images = ["slide1.png", "slide2.png", "slide3.png", "slide4.png", "slide5.png", "slide6.png", "slide7.png", "slide8.png", "slide9.png", "slide10.png"];
-const links = ["https://boresjazz.hu/programok/?render=full", "https://opusjazzclub.hu/koncertek/jambor-armin-quartet-2508", "https://www.facebook.com/people/Jazz-PArt/100084734805068/", "https://www.jazzland.at", "https://www.musikwerkstattwels.at", "https://www.lepirate-rosenheim.de", "https://www.royalgarden.at/programm/", "https://www.tribuene-linz.at", "https://www.cafe-traxlmayr.at/konzerte/", "https://zehnerhaus-badradkersburg.at/veranstaltungskalender/"];
+const images = [
+  "slide1.png", 
+  "slide2.png", 
+  "slide3.png", 
+  "slide4.png", 
+  "slide5.png",
+  "slide6.png", 
+  "slide7.png",
+  "slide8.png", 
+  "slide9.png", 
+  "slide10.png"
+];  // your images
+const links = [
+  "https://boresjazz.hu/programok/?render=full", 
+  "https://opusjazzclub.hu/koncertek/jambor-armin-quartet-2508",
+  "https://www.facebook.com/people/Jazz-PArt/100084734805068/", 
+  "https://www.jazzland.at",
+  "https://www.musikwerkstattwels.at", 
+  "https://www.lepirate-rosenheim.de",
+  "https://www.royalgarden.at/programm/", 
+  "https://www.tribuene-linz.at", 
+  "https://www.cafe-traxlmayr.at/konzerte/",
+  "https://zehnerhaus-badradkersburg.at/veranstaltungskalender/"
+
+];
 
 let current = 0;
 
@@ -23,3 +46,4 @@ carouselImage.addEventListener("click", function(event) {
     window.open(links[current], "_blank");
   }
 });
+
