e5817ff83c98d8ccefa5d1214f45a35a7529463b
laliviation <154061022+laliviation@users.noreply.github.com>
2025-07-06 11:41:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const images = ["img1.jpg", "img2.jpg", "img3.jpg"];
+const links = ["https://site1.com", "https://site2.com", "https://site3.com"];
+let current = 0;
 
+function nextSlide() {
+  current = (current + 1) % images.length;
+  document.getElementById("carouselImage").src = images[current];
+}
+
+function visitLink() {
+  window.open(links[current], "_blank");
+}
