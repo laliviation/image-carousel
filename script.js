9808fee687b9ee429f55cbe59c4be936049df7e2
laliviation <154061022+laliviation@users.noreply.github.com>
2025-07-06 11:59:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const images = ["img1.jpg", "img2.jpg", "img3.jpg"];
-const links = ["https://site1.com", "https://site2.com", "https://site3.com"];
+const images = ["slide1.png", "slide2.png", "slide3.png", "slide4.png", "slide5.png", "slide6.png", "slide7.png", "slide8.png", "slide9.png", "slide10.png"];
+const links = ["https://boresjazz.hu/programok/?render=full", "https://opusjazzclub.hu/koncertek/jambor-armin-quartet-2508", "https://www.facebook.com/people/Jazz-PArt/100084734805068/", "https://www.jazzland.at", "https://www.musikwerkstattwels.at", "https://www.lepirate-rosenheim.de", "https://www.royalgarden.at/programm/", "https://www.tribuene-linz.at", "https://www.cafe-traxlmayr.at/konzerte/", "https://zehnerhaus-badradkersburg.at/veranstaltungskalender/"];
 let current = 0;
 
 function nextSlide() {
